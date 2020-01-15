c548c6791c61e9fde96ce70d178baeab04483d0b
Adam Nagy <an2208@nyu.edu>
2020-01-15 15:06:31 +0100

    working

--- script.js
+++ script.js
@@ -1,21 +1,96 @@
-var calcScreen = document.getElementsByClassName("result").innerHTML;
-console.log(calcScreen);
+let result = 0;
+let buffer = 0;
+let operation = "";
 
-var buttons = document.querySelectorAll(".calc-button");
-console.log(buttons);
-
-
-
-var buttons = document.getElementsByClassName("calc-button");
-buttons.addEventListener("click", determineAction);
-
-function determineAction(value) {
-    if (typeof(value)=="number"){
-        if (screen == 0){
-            screen = "";
-            screen = screen.concat(screen, str(value)) ;
-        }
+document
+  .querySelector(".calculator-container")
+  .addEventListener("click", function(event) {
+    if (Number(event.target.innerText)) {
+      handleNumbers(event.target.innerText);
     } else {
-
+      determineOperation(event.target.innerText);
     }
+  });
+
+function handleNumbers(num) {
+  if (num || result) {
+    if (!result) {
+      result = "";
+    }
+    result = result.toString();
+    num = num.toString();
+    result += num;
+    displayResult();
+  }
+}
+
+function displayResult() {
+  if (result != 0) {
+    document.querySelector(".result").innerHTML = result;
+  } else {
+    document.querySelector(".result").innerHTML = 0;
+  }
+}
+
+function determineOperation(operator) {
+  switch (operator) {
+    case "C":
+      clearDisplay();
+      break;
+    case "←":
+      backSpace();
+      break;
+    case "=":
+      equal();
+      break;
+    case "0":
+      handleNumbers(Number(operator));
+      break;
+    default:
+      changeInput();
+      operation = operator;
+  }
+}
+
+function clearDisplay() {
+  result = 0;
+  displayResult();
+}
+
+function backSpace() {
+  console.log("backspace");
+  if (result != 0) {
+    result = result.toString();
+    result = result.substring(0, result.length - 1);
+    displayResult();
+  }
+}
+
+function changeInput() {
+  if (buffer == 0) {
+    buffer = result;
+    result = 0;
+    displayResult();
+  }
+}
+
+function equal() {
+  switch (operation) {
+    case "+":
+      result = Number(result) + Number(buffer);
+      break;
+    case "-":
+      result = Number(buffer) - Number(result);
+      break;
+    case "×":
+      result = result * buffer;
+      break;
+    case "÷":
+      result = Number(buffer) / Number(result);
+      break;
+      default: clearDisplay();
+  }
+  operation = '';
+  buffer = 0;
+  displayResult();
 }
