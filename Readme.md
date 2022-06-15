db1cb596962cb03b71b2c6c134f642d7fa51f04d
Adam Nagy <nagy.adamn@gmail.com>
2022-06-15 17:49:42 +0200

    Sketch floating point function

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Calculator
+
+## To Do
+[] Add Keyboard support
+[] add floating point support
+
+
+### Floating point
+if there is a floating point already, return
+if there is no number yet, add zero and floating point
+if there is a number, add floating point at the end
+
+at operations, 
+    check if a floating point is dangling at the end
+        yes -> take it out
+    convert the number regularly
--- script.js
+++ script.js
@@ -3,94 +3,102 @@ let buffer = 0;
 let operation = "";
 
 document
-  .querySelector(".calculator-container")
-  .addEventListener("click", function(event) {
-    if (Number(event.target.innerText)) {
-      handleNumbers(event.target.innerText);
-    } else {
-      determineOperation(event.target.innerText);
-    }
-  });
+	.querySelector(".calculator-container")
+	.addEventListener("click", function (event) {
+		if (Number(event.target.innerText)) {
+			handleNumbers(event.target.innerText);
+		} else {
+			determineOperation(event.target.innerText);
+		}
+	});
 
 function handleNumbers(num) {
-  if (num || result) {
-    if (!result) {
-      result = "";
+	if (num || result) {
+		if (!result) {
+			result = "";
+		}
+		result = result.toString();
+		num = num.toString();
+		result += num;
+		displayResult();
+	}
+}
+
+function handleFloatingPoint () {
+    if (num || result) {
+
     }
-    result = result.toString();
-    num = num.toString();
-    result += num;
-    displayResult();
-  }
 }
 
 function displayResult() {
-  if (result != 0) {
-    document.querySelector(".result").innerHTML = result;
-  } else {
-    document.querySelector(".result").innerHTML = 0;
-  }
+	if (result != 0) {
+		document.querySelector(".result").innerHTML = result;
+	} else {
+		document.querySelector(".result").innerHTML = 0;
+	}
 }
 
 function determineOperation(operator) {
-  switch (operator) {
-    case "C":
-      clearDisplay();
-      break;
-    case "←":
-      backSpace();
-      break;
-    case "=":
-      equal();
-      break;
-    case "0":
-      handleNumbers(Number(operator));
-      break;
-    default:
-      changeInput();
-      operation = operator;
-  }
+	switch (operator) {
+		case "C":
+			clearDisplay();
+			break;
+		case "←":
+			backSpace();
+			break;
+		case "=":
+			equal();
+			break;
+		case "0":
+			handleNumbers(Number(operator));
+			break;
+		case ".":
+			handleFloatingPoint();
+		default:
+			changeInput();
+			operation = operator;
+	}
 }
 
 function clearDisplay() {
-  result = 0;
-  displayResult();
+	result = 0;
+	displayResult();
 }
 
 function backSpace() {
-  if (result != 0) {
-    result = result.toString();
-    result = result.substring(0, result.length - 1);
-    displayResult();
-  }
+	if (result != 0) {
+		result = result.toString();
+		result = result.substring(0, result.length - 1);
+		displayResult();
+	}
 }
 
 function changeInput() {
-  if (buffer == 0) {
-    buffer = result;
-    result = 0;
-    // displayResult();
-  }
+	if (buffer == 0) {
+		buffer = result;
+		result = 0;
+		// displayResult();
+	}
 }
 
 function equal() {
-  switch (operation) {
-    case "+":
-      result = Number(result) + Number(buffer);
-      break;
-    case "-":
-      result = Number(buffer) - Number(result);
-      break;
-    case "×":
-      result = Number(result) * Number(buffer);
-      break;
-    case "÷":
-      result = Number(buffer) / Number(result);
-      break;
-    default:
-      return;
-  }
-  operation = "";
-  buffer = 0;
-  displayResult();
+	switch (operation) {
+		case "+":
+			result = Number(result) + Number(buffer);
+			break;
+		case "-":
+			result = Number(buffer) - Number(result);
+			break;
+		case "×":
+			result = Number(result) * Number(buffer);
+			break;
+		case "÷":
+			result = Number(buffer) / Number(result);
+			break;
+		default:
+			return;
+	}
+	operation = "";
+	buffer = 0;
+	displayResult();
 }
